f15cb98501415f31836d072326fb78a115136a41
Sviatoslav Kryachev <same92121@gmail.com>
2022-09-02 19:52:23 +0300

    new

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+//const currentYear=2022;
+
+//let lifeAge = prompt ('Введите возраст который хотите прожить')
+//const age= prompt ('Введите Ваш текущий возраст');
+//let a= lifeAge - age 
+//console.log ("Какие планы на ближайшие" + " " +a + "лет?")
+
+//const firstName= "Sviatoslav"
+//const lastName = prompt("Введите фамилию")
+//alert (firstName + " " + lastName)
